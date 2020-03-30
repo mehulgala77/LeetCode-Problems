fbcfafe18d32a011e818679a4b6fc3efb781cd4e
“mehulgala77” <mehulgala007@gmail.com>
2020-03-30 18:03:03 +0530

    1st Classic Algorithm (Valid Parenthesis) added.

---
+++ Classic Algorithms/Valid Parenthesis.py
@@ -0,0 +1,52 @@
+
+def isValid(s):
+    stack = []
+
+    for item in s:
+
+        if item in ["(", "{", "["]:
+
+            stack.append(item);
+
+        elif len(stack) == 0:
+
+            return False
+
+        else:
+
+            poppedItem = stack.pop()
+
+            if item == ")" and poppedItem != "(":
+                return False
+
+            if item == "}" and poppedItem != "{":
+                return False
+
+            if item == "]" and poppedItem != "[":
+                return False
+
+    else:
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+
+# Driver Code
+
+# True
+result = isValid("([])")
+print(result)
+
+# False
+result = isValid("(((")
+print(result)
+
+# True
+result = isValid("(){}[]")
+print(result)
+
+# False
+result = isValid("({}([]")
+print(result)
